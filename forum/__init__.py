11df1bce30ff68f55c478e4307d991142415247e
AnupamKris <anupamkris13262@gmail.com>
2020-12-30 21:27:57 +0530

    NewUpdate

--- forum/__init__.py
+++ forum/__init__.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask import Flask
-from flask_pywebpush import WebPush
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '89ry2uqyr892n21vn32k4j'
