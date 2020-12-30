07fb09f8f05627d185b38e87b8aab016d56fa02b
AnupamKris <anupamkris13262@gmail.com>
2020-12-30 21:36:39 +0530

    asd

--- forum/__init__.py
+++ forum/__init__.py
@@ -8,7 +8,6 @@ app.config['SECRET_KEY'] = '89ry2uqyr892n21vn32k4j'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-push = WebPush(app)
 # login_manager = LoginManager(app)
 # login_manager.login_view = 'login'
 
